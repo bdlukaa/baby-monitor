ad8147bc7271b7baf0254f7444803df301a8be47
Illumimatt <deters.mk@sempreceub.com>
2025-05-22 09:49:43 -0300

    Adicionando cores da logotipo no Color.kt

--- app/app/src/main/java/com/babymonitor/ui/theme/Color.kt
+++ app/app/src/main/java/com/babymonitor/ui/theme/Color.kt
@@ -2,6 +2,12 @@ package com.babymonitor.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val AzulPastel = Color(0xFFA7D8F0)
+val RosaPastel = Color(0xFFF7C3D6)
+val AmareloPastel = Color(0xFFF9E29C)
+val AzulContorno = Color(0xFF7D8FCB)
+val FundoPastel = Color(0xFFFCF9EB)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
